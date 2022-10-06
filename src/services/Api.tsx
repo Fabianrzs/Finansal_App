e43e24e9999247f8647271726ad9e5ac73369e2d
Fdamirez <fabianramirez0072@gmail.com>
2022-10-06 09:19:06 -0500

    feat api whith axios

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,3 +1,59 @@
-export default function hola() {
-  return 'hola'
+import axios, { AxiosRequestConfig } from "axios";
+
+export default {
+  async apiRestGet<TEntity>(path:string, body:{}): Promise<TEntity> {
+    try {
+      const requestOptions: AxiosRequestConfig = {
+        params: body
+      }
+      return await axios.get(path, requestOptions)
+    } catch (err) {
+      console.log(err);
+      throw { err }
+    }
+  },
+  async apiRestPost<TEntity>(path:string, body:{}): Promise<TEntity>{
+    try {
+      const requestOptions: AxiosRequestConfig = {
+        params: body
+      }
+      return await  axios.post(path, requestOptions)
+    }catch (err){
+      console.log(err);
+      throw {err}
+    }
+  },
+  async apiRestPut<TEntity>(path:string, body:{}): Promise<TEntity>{
+    try {
+      const requestOptions: AxiosRequestConfig = {
+        params: body
+      }
+      return await  axios.put(path, requestOptions)
+    }catch (err){
+      console.log(err);
+      throw {err}
+    }
+  },
+  async  apiRestpatch<TEntity>(path:string, body:{}): Promise<TEntity>{
+    try {
+      const requestOptions: AxiosRequestConfig = {
+        params: body
+      }
+      return await  axios.patch(path, requestOptions)
+    }catch (err){
+      console.log(err);
+      throw {err}
+    }
+  },
+  async  apiRestDelete<TEntity>(path:string, body:{}): Promise<TEntity>{
+    try {
+      const requestOptions: AxiosRequestConfig = {
+        params: body
+      }
+      return await  axios.delete(path, requestOptions)
+    }catch (err){
+      console.log(err);
+      throw {err}
+    }
+  },
 }
