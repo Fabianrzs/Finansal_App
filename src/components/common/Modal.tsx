14ae625b58284939cb1fc36801ea741f2afc0d47
Fdamirez <fabianramirez0072@gmail.com>
2022-10-06 11:35:35 -0500

    feat modal

---
+++ src/components/common/Modal.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Modal as ModalNative, ModalBaseProps, Pressable, Text, View } from "react-native";
+import { styles } from "../../styles/components/common/Modal.style";
+
+interface ModalProps{
+  title?: string
+  body:  JSX.Element
+  animated?: boolean;
+  animationType?: 'none' | 'slide' | 'fade';
+  transparent?: boolean;
+  visible: boolean;
+  setVisible: Function;
+}
+
+export default function Modal (props: ModalProps){
+  const {title ,animationType, visible, transparent, body, animated, setVisible } = props
+  return(
+    <View>
+      <ModalNative
+        animated={animated}
+        animationType={animationType}
+        transparent={transparent}
+        visible={visible}
+        statusBarTranslucent={true}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View>
+              <Text style={styles.titleStyle}>{title}</Text>
+            </View>
+            <View style={styles.modalBody}>
+              {body}
+            </View>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setVisible(!visible)}
+            >
+              <Text style={styles.textStyle}>Aceptar</Text>
+            </Pressable>
+          </View>
+        </View>
+      </ModalNative>
+    </View>
+  )
+}
--- src/screens/App.tsx
+++ src/screens/App.tsx
@@ -1,8 +1,47 @@
-import Login from "./user/login";
+import React, { useState } from "react";
+import { StyleSheet, Text, Pressable, View } from "react-native";
+import Modal from "../components/common/Modal";
 
 const App = () => {
-  return(
-      <Login />
+  const [modalVisible, setModalVisible] = useState(false);
+  const body = (
+    <View>
+      <Text>Me das el si?</Text>
+    </View>
   )
-}
+  
+  return (
+    <View style={styles.centeredView}>
+      <Modal body={body} setVisible={setModalVisible}
+             animated={true} animationType={"fade"}
+             transparent={true} visible={modalVisible}
+             title={"Hola Como Estas?"}
+      />
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text>Show Modal</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+});
+
 export default App;
