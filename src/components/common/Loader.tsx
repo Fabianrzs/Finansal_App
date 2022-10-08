63f708a44bbdec1c4243c62fca3c28bac28ceafb
Fdamirez <fabianramirez0072@gmail.com>
2022-10-08 12:19:13 -0500

    Update Loader.tsx

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { ActivityIndicator, View } from "react-native";
-import {styles} from "../../styles/components/common/Loader.style";
+import {styles} from "../../themes/components/common/Loader.style";
 
 export default function Loader(){
   return(
